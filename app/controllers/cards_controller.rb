f7b8ac6f5a95688dc37293b60be9bde4e7f21f7b
Hugh Mitton <hm@hughmitton.com>
2016-12-06 18:47:28 +0100

    somewhat functional

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,30 @@
 class CardsController < ApplicationController
 
-  def show
+  def show(card_id = Card.all.sample.id)
+    @new_card = Card.find(card_id)
   end
 
-  def select
-    @cards = Card.all
-    @new_card = @cards.sample
+  def swipe
+    # @swipe = Swipe.new(card_id: params[:card_id], liked: params[:liked])
+    cookies[:liked] = []
+    cookies[:disliked] = []
+    if params[:liked] == 1
+      cookies[:liked] << params[:card_id].to_i
+    else
+      cookies[:disliked] << params[:card_id].to_i
+    end
+    next_card
+  end
+
+  def next_card
+    @all_card_ids = []
+    Card.all.each { |x| @all_card_ids << x.id }
+    @seen = cookies[:liked] + cookies[:disliked]
+    if @all_card_ids - @seen == []
+      redirect_to out_of_cards_path
+    else
+      @next_card_id = (@all_card_ids - @seen).sample
+      redirect_to cards_show_path(@next_card_id)
+    end
   end
 end
