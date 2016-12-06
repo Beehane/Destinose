aee912ec4df5a94dc09c6d2026032eee9fd915b0
Hugh Mitton <hm@hughmitton.com>
2016-12-06 15:32:48 +0100

    changed results to recommendations

--- app/models/result_card.rb
+++
@@ -1,4 +0,0 @@
-class ResultCard < ApplicationRecord
-  belongs_to :Result
-  belongs_to :Card
-end
--- db/migrate/20161206112918_add_result_card_reference_to_result.rb
+++
@@ -1,5 +0,0 @@
-class AddResultCardReferenceToResult < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :results, :result_card, foreign_key: true
-  end
-end
