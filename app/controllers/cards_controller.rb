d6b59ed442ff0c3fdae6dd641e0bb196a6fcc884
Hugh Mitton <hm@hughmitton.com>
2016-12-06 19:41:48 +0100

    added destroy_all to seed

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,25 +6,32 @@ class CardsController < ApplicationController
 
   def swipe
     # @swipe = Swipe.new(card_id: params[:card_id], liked: params[:liked])
-    cookies[:liked] = []
-    cookies[:disliked] = []
-    if params[:liked] == 1
-      cookies[:liked] << params[:card_id].to_i
+    parse_cookies
+    if params[:liked].to_i == 1
+      @liked << params[:card_id].to_i
     else
-      cookies[:disliked] << params[:card_id].to_i
+      @disliked << params[:card_id].to_i
     end
+    cookies[:liked] = @liked.to_json
+    cookies[:disliked] = @disliked.to_json
     next_card
+  end
+
+  def parse_cookies
+    @liked = JSON.parse(cookies[:liked] || "[]")
+    @disliked = JSON.parse(cookies[:disliked] || "[]")
   end
 
   def next_card
     @all_card_ids = []
     Card.all.each { |x| @all_card_ids << x.id }
-    @seen = cookies[:liked] + cookies[:disliked]
-    if @all_card_ids - @seen == []
+    @seen = @liked + @disliked
+    if (@all_card_ids - @seen) == []
       redirect_to out_of_cards_path
     else
       @next_card_id = (@all_card_ids - @seen).sample
       redirect_to cards_show_path(@next_card_id)
     end
   end
+
 end
