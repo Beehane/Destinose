05a200e3d2d1effa0953d244acb39c8b1d8af15c
Beehane <b.hanae@gmail.com>
2016-12-06 17:01:18 +0100

    A perusing seed file with Hugh's wanderings in the world

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,124 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Card.create(
+  name: "Auckland, New Zealand",
+  lat: -36.84846,
+  lng: 174.763332,
+  description: "A view from Mount Roskill",
+  image: "0013_w7vqx8.jpg",
+  )
+
+Card.create(
+  name: "Dar Es Salam, Tanzania",
+  lat: -6.792354,
+  lng: 39.208328,
+  description: "A view from the center",
+  image: "0002_kn7mgv.jpg",
+  )
+
+Card.create(
+  name: "Magnet Bay, New Zealand",
+  lat: -43.84356,
+  lng: 172.739434,
+  description: "A paradise for surfers",
+  image: "0003_o4wppe.jpg",
+  )
+
+Card.create(
+  name: "Udaipur, India",
+  lat: 24.585445,
+  lng: 73.712479,
+  description: "A spiritural retreat in India",
+  image: "0004_yfslxz.jpg",
+  )
+
+Card.create(
+  name: "Auckland, New Zealand",
+  lat: -36.84846,
+  lng: 174.763332,
+  description: "A view from Mount Roskill",
+  image: "0013_w7vqx8.jpg",
+  )
+
+Card.create(
+  name: "Semien Mountains, Ethiopia",
+  lat: 13.301634,
+  lng: 38.295738,
+  description: "A resourcing trip for nature lovers",
+  image: "0013_w7vqx8.jpg",
+  )
+
+Card.create(
+  name: "Kendwa, Zanzibar",
+  lat: -6.165917,
+  lng: 39.202641,
+  description: "A peaceful pool and beach retreat",
+  image: "0006_sifyoy.jpg",
+  )
+
+Card.create(
+  name: "Tempelhof Berlin, Germany",
+  lat: 52.457553,
+  lng: 13.391103,
+  description: "The starting point of your journey to Germany",
+  image: "0007_tdobfx.jpg",
+  )
+
+Card.create(
+  name: "Wellington, New Zealand",
+  lat: -41.28646,
+  lng: 174.776236,
+  description: "A view from Mount Roskill",
+  image: "0008_mmoahn.jpg",
+  )
+
+Card.create(
+  name: "Bronte Beach Sydney, Australia",
+  lat: -33.86882,
+  lng: 151.209296,
+  description: "The next coolest beach",
+  image: "0009_tzla0d.jpg",
+  )
+
+Card.create(
+  name: "Joshua Tree CA, United States of America",
+  lat: 34.134728,
+  lng: -116.313066,
+  description: "The Good, the Bad and the Ugly, or so...",
+  image: "0010_iccdep.jpg",
+  )
+
+Card.create(
+  name: "Machu Picchu, Peru",
+  lat: -13.163141,
+  lng: -72.544963,
+  description: "Your one and only immersion into the sacred world of Incas",
+  image: "0013_w7vqx8.jpg",
+  )
+
+Card.create(
+  name: "Valle de Cocora, Colombia",
+  lat: 4.638054,
+  lng: -75.487783,
+  description: "Nature offers you its finest",
+  image: "0012_cyh4ik.jpg",
+  )
+
+Card.create(
+  name: "Old Town, Cartagena, Colombia",
+  lat: 10.391049,
+  lng: -75.479426,
+  description: "Witness history in the colorful colonial streets of Cartagena",
+  image: "0012_cyh4ik.jpg",
+  )
+
+Card.create(
+  name: "Asakusa, Tokyo, Japan",
+  lat: 35.711877,
+  lng: 139.796697,
+  description: "Deep dive into vibrant capital of Japan",
+  image: "0012_cyh4ik.jpg",
+  )
+
