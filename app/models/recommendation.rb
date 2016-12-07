35a5b155d76256d78a78011a63c902cf3967dc86
Beehane <b.hanae@gmail.com>
2016-12-07 12:39:20 +0100

    Displaying a Huugh's map

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,6 +1,6 @@
 class Recommendation < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :swipes, through: :user
-  geocoded_by :location
-  after_validation :geocode, if: :location_changed?
+  geocoded_by :latitude
+  after_validation :geocode, if: :latitude_changed?
 end
