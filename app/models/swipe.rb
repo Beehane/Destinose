eb5c63687849a1363e05a2305af37f3374d27c21
Hugh Mitton <hm@hughmitton.com>
2016-12-08 13:28:57 +0100

    fixing through model

--- app/models/swipe.rb
+++ app/models/swipe.rb
@@ -1,7 +1,6 @@
 class Swipe < ApplicationRecord
   belongs_to :card
   belongs_to :recommendation
-  belongs_to :user, through: :recommendation
 
   validates :card, presence: :true
   validates :user, presence: :true
