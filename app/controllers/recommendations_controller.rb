e72c22bc4c395f454f303540163555c1ab3efa5b
Hugh Mitton <hm@hughmitton.com>
2016-12-08 13:11:45 +0100

    swapped reco save method to helpers

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -44,20 +44,4 @@ class RecommendationsController < ApplicationController
     @disliked = JSON.parse(cookies[:disliked])
   end
 
-  def save_to_recommendation
-    parse_cookies
-    reco = Recommendation.new(user_id: current_user.id)
-    @liked.each do |x|
-      card = Card.find(x)
-      swipe = Swipe.new(card: card, user_id: current_user, liked: 1, recommendation: reco)
-      swipe.save
-    end
-    @disliked.each do |x|
-      card = Card.find(x)
-      swipe = Swipe.new(card: card, user: current_user, liked: 0, recommendation: reco)
-      swipe.save
-    end
-    reco.save
-    flash[:notice] = 'result saved successfully'
-  end
 end
--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,2 +1,18 @@
 module RecommendationsHelper
+  def save_to_recommendation
+    parse_cookies
+    reco = Recommendation.new(user: current_user)
+    @liked.each do |x|
+      card = Card.find(x)
+      swipe = Swipe.new(card: card, user: current_user, liked: 1, recommendation: reco)
+      swipe.save
+    end
+    @disliked.each do |x|
+      card = Card.find(x)
+      swipe = Swipe.new(card: card, user: current_user, liked: 0, recommendation: reco)
+      swipe.save
+    end
+    reco.save
+    flash[:notice] = 'result saved successfully'
+  end
 end
