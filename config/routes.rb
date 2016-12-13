67ade7f0b485f0a2d15f71ed71928741b732d0ec
Beehane <b.hanae@gmail.com>
2016-12-13 15:10:56 +0100

    Landing page layout - temporary route to be changed to permanent route

--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,8 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Temporary routes
+    get '/landing', to: 'pages#landing'
+
 end
