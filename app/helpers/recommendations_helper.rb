ace9460055c653cf10ffd6694dca3a21a86a8c6c
Hugh Mitton <hm@hughmitton.com>
2016-12-15 02:38:48 +0100

    itinerary cards semi-formed, rome2rio works

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,3 +1,16 @@
 module RecommendationsHelper
 
+  def rome2rio(trip_array)
+    trip_array.each_cons(2) do |x, y|
+      card1 = Card.find(x)
+      card2 = Card.find(y)
+      url = "http://free.rome2rio.com/api/1.4/json/Search?key=#{ENV['ROME2RIO']}&oPos=#{card1.latitude},#{card1.longitude}&dPos=#{card2.latitude},#{card2.longitude}"
+      file = open(url).read
+      rome2rio_query = JSON.parse(file)
+      raise
+    end
+
+
+  end
+
 end
