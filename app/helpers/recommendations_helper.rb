b794b241c4b71f2fa40ffba09cbd4c14df7a9fc9
Hugh Mitton <hm@hughmitton.com>
2016-12-15 03:02:43 +0100

    rome2rio work

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,13 +1,27 @@
 module RecommendationsHelper
 
-  def rome2rio(trip_array)
-    trip_array.each_cons(2) do |x, y|
+  def rome2rio(two_points)
+    two_points.each_cons(2) do |x, y|
       card1 = Card.find(x)
       card2 = Card.find(y)
       url = "http://free.rome2rio.com/api/1.4/json/Search?key=#{ENV['ROME2RIO']}&oPos=#{card1.latitude},#{card1.longitude}&dPos=#{card2.latitude},#{card2.longitude}"
       file = open(url).read
-      rome2rio_query = JSON.parse(file)
-      raise
+      r2r_query = JSON.parse(file)
+      @transport1 = {
+        name: r2r_query["routes"][0]["name"],
+        price: r2r_query["routes"][0]["indicativePrices"][0]["price"],
+        url: "https://www.rome2rio.com/s/#{card1.latitude},#{card1.longitude}/#{card2.latitude},#{card2.longitude}"
+      }
+      @transport2 = {
+        name: r2r_query["routes"][1]["name"],
+        price: r2r_query["routes"][1]["indicativePrices"][0]["price"],
+        url: "https://www.rome2rio.com/s/#{card1.latitude},#{card1.longitude}/#{card2.latitude},#{card2.longitude}"
+      } unless r2r_query["routes"][1].nil?
+      @transport3 = {
+        name: r2r_query["routes"][2]["name"],
+        price: r2r_query["routes"][2]["indicativePrices"][0]["price"],
+        url: "https://www.rome2rio.com/s/#{card1.latitude},#{card1.longitude}/#{card2.latitude},#{card2.longitude}"
+      } unless r2r_query["routes"][2].nil?
     end
 
 
